750230ff92752e1017ba491ac6a4c249a595903b
Farihaakter <yellowfariha@gmail.com>
2021-04-17 19:43:04 +0600

    added changes

--- index.js
+++ index.js
@@ -100,17 +100,18 @@ client.connect(err => {
       })
 
       app.get('/orderlist', (req, res) => {
-        adminCollection.find({ email: req.query.email })
-          .toArray((err, admins) => {
-            console.log(admins)
-            if(admins.length === 0){
-                res.send([])
-            }
-            bookingsCollection.find()
-            .toArray((err, documents)=>{
-                res.send(documents)
-            })
-          })
+          console.log(req.query.email)
+        // adminCollection.find({ email: req.query.email })
+        //   .toArray((err, admins) => {
+        //     console.log(admins)
+        //     if(admins.length === 0){
+        //         res.send([])
+        //     }
+        //     bookingsCollection.find()
+        //     .toArray((err, documents)=>{
+        //         res.send(documents)
+        //     })
+        //   })
       })
 
 
